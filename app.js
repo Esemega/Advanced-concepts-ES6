80a079f3781b3700aedc452ff694acdc3aed7405
Esemega <sogaral89@gmail.com>
2020-12-10 22:35:35 +0100

    hasId function done

--- app.js
+++ app.js
@@ -1,1 +1,21 @@
-console.log("A programar!")
+/**
+ * Check if a object have a property named "id".
+ * @param {Object} param0 Object
+ */
+const hasId = ({id}) =>  !(id === undefined);
+
+const house = {
+    id: 4,
+    price: 45000,
+    meters: 450
+}
+
+const people = {
+    name: "Juan",
+    age: 67
+}
+
+console.log(house);
+console.log("Have property id?", hasId(house));
+console.log(people);
+console.log("Have property id?", hasId(people));
